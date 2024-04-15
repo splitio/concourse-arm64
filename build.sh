e27a4f13f17d46d5c94fe2467a312440130bebb0
Mariano Arago <mariano.arago@split.io>
2024-04-15 18:02:43 -0300

    Fix GHA

--- build.sh
+++ build.sh
@@ -43,7 +43,7 @@ buildConcourseResourceDocker() {
     --build-arg ${_build_arg_type}_resource_version=${_version} \
     --platform linux/arm64 \
     --tag $DOCKER_REGISTRY_BASE/concourse-${_type}-resource:${_version} \
-    -f resource-types/Dockerfile-${_type}-resource
+    -f resource-types/Dockerfile-${_type}-resource .
 
   if [ "$SHOULD_PUSH" = "true" ]; then
     docker push $DOCKER_REGISTRY_BASE/concourse-${_type}-resource:${_version}
@@ -88,7 +88,6 @@ for task in dcind:1.0.0 oci-build:0.9.0; do
   (cd ./external-tasks/$_t && docker buildx build \
     --platform linux/arm64 \
     --build-arg ${_b}_task_version=${_v} \
-    --tag $DOCKER_REGISTRY_BASE/concourse-${_t}-task:latest \
     --tag $DOCKER_REGISTRY_BASE/concourse-${_t}-task:${_v}) .
 
   if [ "$SHOULD_PUSH" = "true" ]; then
